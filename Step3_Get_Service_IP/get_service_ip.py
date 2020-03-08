81fee624e5f76b2e996670613c585b5fb8d10c30
Ethan Zhi <zhiyhappy@163.com>
2020-03-08 10:55:58 +0800

    15th commit

---
+++ Step3_Get_Service_IP/get_service_ip.py
@@ -0,0 +1,16 @@
+import requests
+import json
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+headers = {
+    'header-api-key': 'yr0it2ejia__D9b52uxYTvaLIERbt8A5R4VHy9RJkVvuaY4EGtPY',
+}
+
+params = (
+    ('fwType', 'gpcs_remote_network'),
+    ('addrType', 'public_ip'),
+)
+
+response = requests.get('https://api.gpcloudservice.com/getAddrList/latest', headers=headers, params=params, verify=False)
+output = response.content
